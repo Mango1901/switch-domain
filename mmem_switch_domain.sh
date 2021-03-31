a6c273e5f7c21fdc8c1fe93fff7c71cb12da3b29
Mango1901 <hieu.tuhai2001@gmail.com>
2021-03-31 14:39:05 +0700

    save code

---
+++ mmem_switch_domain.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mysql_user=$1
+mysql_password=$2;
+mysql_db=$3;
+domain_switch=$4;
+mysql_port=$5;
+mysql_host=$6;
+
+if [ -z "${mysql_host}"  ]; then
+    mysql_host=127.0.0.1
+fi
+if [ -z "${mysql_port}"  ]; then
+    mysql_host=3306
+fi
+
+mysql -u "${mysql_user}" -p"${mysql_password}" -h "${mysql_host}" -P "${mysql_port}" \
+  -e "use '${mysql_db}'" \
+  -e "UPDATE core_config_data SET value = '${domain_switch}' WHERE path = 'web/secure/base_url';" \
+  -e "UPDATE core_config_data SET value = '${domain_switch}' WHERE path = 'web/unsecure/base_url';" \
+  -e "UPDATE brand_entity_settings SET value = '${domain_switch}' WHERE path = 'web/secure/base_url';" \
+  -e "UPDATE brand_entity_settings SET value = '${domain_switch}' WHERE path = 'web/unsecure/base_url';" \
+  -e "UPDATE core_config_data SET value = '0' WHERE path = 'admin/url/use_custom';" \
+  -e "UPDATE core_config_data SET value = '0' WHERE path = 'eb/secure/use_in_frontend';" \
+  -e "UPDATE core_config_data SET value = '0' WHERE path = 'web/secure/use_in_adminhtml';"
